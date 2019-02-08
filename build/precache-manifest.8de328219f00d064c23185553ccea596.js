ebbfd0249a3659b5ca90b10ab1fcbc693ddf7a15
davidkensell <d.kensell@pixelz.com>
2019-02-08 10:55:28 -0800

    chore: build and deploy to github pages

---
+++ build/precache-manifest.8de328219f00d064c23185553ccea596.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f8edba5fc82bf39d15",
+    "url": "/myreads/static/css/main.0246ff6d.chunk.css"
+  },
+  {
+    "revision": "63f8edba5fc82bf39d15",
+    "url": "/myreads/static/js/main.63f8edba.chunk.js"
+  },
+  {
+    "revision": "61407f437a1296ed0362",
+    "url": "/myreads/static/js/1.61407f43.chunk.js"
+  },
+  {
+    "revision": "1b7e853e01d73a6cc24d",
+    "url": "/myreads/static/js/runtime~main.1b7e853e.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "29072c2d12c898cdad6e25a1f2893ebe",
+    "url": "/myreads/index.html"
+  }
+];
